fcfc2ff47c6a497bfb4e0217acaddac004714f5a
tishaas <112718668+tishaas@users.noreply.github.com>
2024-02-05 11:37:38 +0530

    change12

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,8 +14,9 @@ export const register = async (req,res,next)=>{
         //console.log("Token",token)
 
         const newUser = new User({
-            username:req.body.username,
-            email:req.body.email,
+            //username:req.body.username,
+            //email:req.body.email,
+           ...req.body,
             password:hash,
             Token:token
         })
@@ -47,10 +48,11 @@ export const login = async (req,res,next)=>{
             httpOnly:true,
         }).status(200).json({
             status: true,
-            data: {...otherDetails},
+            data: {deatils:{...otherDetails},isAdmin},
             message:  "User has been created"
           });
           return;
+          
     }catch(error){
         console.log(error)
 
