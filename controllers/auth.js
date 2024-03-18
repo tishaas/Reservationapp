d1b7186d99ae711741918bfe63eaa471cf0a3c02
tishaas <tishasurati24@gmail.com>
2024-03-18 15:33:09 +0530

    Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,9 +36,10 @@ export const register = async (req,res,next)=>{
 
 export const login = async (req,res,next)=>{
     try{
-        const user = await User.findOne({username:req.body.username})
+        console.log(req.body.email);
+        const user = await User.findOne({username:req.body.email})
         if(!user) return next(createError(404,"User not found!!"))
-
+         
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
         if(!isPasswordCorrect) return next(createError(400,"Wrong Password or username!!"))
     
