b4fe80cafd95296982da01852a9d4b4c85abcded
tishaas <tishasurati24@gmail.com>
2024-03-21 17:37:09 +0530

    Up4

--- controllers/room.js
+++ controllers/room.js
@@ -23,7 +23,7 @@ export const createRoom = async(req,res,next)=>{
 
 export const updateRoom =async (req,res,next)=>{
     try{
-        const updatedRoom =await Hotel.findByIdAndUpdate(req.params.id,
+        const updatedRoom =await Room.findByIdAndUpdate(req.params.id,
             {$set:req.body},{new:true})
         res.status(200).json(updatedRoom)
     }catch(error){
@@ -47,7 +47,7 @@ export const deleteRoom =async (req,res,next)=>{
     try{
         await Room.findOneAndDelete(req.params.id,{$set:req.body})
         try{
-         await Hotel.findByIdAndUpdate(hotelId ,
+         await Room.findByIdAndUpdate(hotelId ,
           {$pull:{rooms:req.params.id}
          })
       }catch(err){   
@@ -62,7 +62,7 @@ export const deleteRoom =async (req,res,next)=>{
 
 export const getRooms =async (req,res,next)=>{
     try{
-        const rooms =await Hotel.find()
+        const rooms =await  Room.find()
         res.status(200).json(rooms)
     }catch(error){
         next(error)
@@ -71,7 +71,7 @@ export const getRooms =async (req,res,next)=>{
 
 export const getRoom =async (req,res,next)=>{
     try{
-        const room =await Hotel.findById(req.params.id)
+        const room =await Room.findById(req.params.id)
         res.status(200).json(room)
     }catch(error){
         res.status(500).json(error)
--- models/User.js
+++ models/User.js
@@ -19,6 +19,7 @@ const UserSchema =new mongoose.Schema({
       },
       img: {
         type: String,
+        default:""
       },
       city: {
         type: String,
